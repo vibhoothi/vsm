217892387f43b28343ea592282b49aa9495e568a
Vibhoothi <vibhoothiiaanand@gmail.com>
2019-05-02 01:28:00 +0530

    vsm: Add Seach function
        - Add search function for calculating Vector space model
        - Take the corpus query from user
        - Calculate Precision and recall of the document
        - and more.....

--- vector_space_model.py
+++ vector_space_model.py
@@ -64,6 +64,58 @@ def calc_tfidf( func_tfidf, func_tokenzied, func_posting_lists):
                     func_tfidf[buffer1][token_buffer] = tf * idf
         buffer1 += 1
 
+def search(func_query, func_posting_lists):
+    tokenized_query = nltk.word_tokenize(func_query)
+    normalized_query = [ stemmer.stem(word.lower()) for word in tokenized_query]
+    normalized_buffer = [stemmer.stem(word.lower())  
+                             for word in tokenized_query
+                        ]
+    for buffer in range(0, len(normalized_query)):
+            if normalized_query[buffer] not in func_posting_lists:
+                buffer2 = difflib.get_close_matches(normalized_query[buffer], func_posting_lists)
+                if len(buffer2) != 0:
+                    special_corpus_query.append(buffer2[0])
+            else:
+                    special_corpus_query.append(normalized_query[buffer])
+            
+    query_tfidf= {}
+    for buffer3 in special_corpus_query:
+        if buffer3 in func_posting_lists:
+            idf = math.log10( float( len(files))/len(set(func_posting_lists[buffer3] )))
+            query_tfidf[buffer3] = (1 + math.log10(special_corpus_query.count(buffer3)))*idf
+    
+    weights_buffer = {}
+    relavant_buffer = {}
+    weight_buffer = 0
+    for buffer in range(0, len(files)):
+        if func_query in open(files[buffer],"r",encoding='utf-8', errors='ignore').read().lower():        
+            relavant_buffer[buffer] = 1
+
+        for buffer3 in special_corpus_query:
+            if buffer3 in func_posting_lists:
+                if (buffer+1) in func_posting_lists[buffer3]:
+                    count = len(func_posting_lists[buffer3][buffer+1])
+                    if count != 0:
+                        tf = 1 + math.log10(count)
+                        idf = math.log10( float(len(files))/len(set(func_posting_lists[buffer3])))
+                        weight_buffer += query_tfidf[buffer3]*(tf*idf)
+
+        list_buffer = list( query_tfidf.values())
+        squareroot_sum_query = reduce(lambda x,y: x+y*y, [list_buffer[:1][0]**2]+list_buffer[1:])
+        list_buffer = list(tfidf[buffer+1].values())
+        squareroot_sum_doc = reduce(lambda x,y: x+y*y,[list_buffer[:1][0]**2]+list_buffer[1:])
+        weight_buffer = weight_buffer/((squareroot_sum_query**(1/2))*(squareroot_sum_doc**(1/2)))
+        if weight_buffer != 0:
+            weights[buffer] = weight_buffer
+        
+    relevant_documents = len(relavant_buffer)
+    retrieved_documents = min(len(weights), 20)
+    relevant_retrieved_documents = min(len(set(relavant_buffer.keys()) & set(weights.keys())), retrieved_documents )
+    precision = relevant_retrieved_documents / float(retrieved_documents)
+    recall = relevant_retrieved_documents / float(relevant_documents)
+    print( "Precision: "+ str(precision))
+    print( "Recall: " + str(recall))
+
 def find_cosine_similarity(func_weights):
     buffer = 0
     dictionary_keys = list( sorted(func_weights, key=func_weights.__getitem__, reverse=True))
@@ -81,7 +133,10 @@ def pretty_tfidf(func_tfidf):
 def main():
     fetch_files()
     token_normalize( tokenzied, stemmer )
+    create_posting_list( tokenzied, posting_lists )
     calc_tfidf(tfidf, toenzied, posting_lists )
+    query = input("Corpus: ")
+    search(query, posting_lists)
     find_cosine_similarity(weights)
     pretty_tfidf(tfidf)
 
